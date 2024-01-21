4cbd5b0ef79b9727fa782167f797b187b57d4f4d
Arnold Truong <vinhhao.truong.52@gmail.com>
2024-01-21 23:07:42 +0700

    Update nav link hover animation

--- components/layouts/Navigation/index.tsx
+++ components/layouts/Navigation/index.tsx
@@ -9,25 +9,27 @@ import { useMotionValueEvent, useScroll, motion } from "framer-motion";
 
 const Navigation = () => {
   const { scrollY } = useScroll();
+
   const [isScrollDown, setIsScrollDown] = useState<boolean>(false);
   const [position, setPosition] = useState<number>(0);
+  const [hoveredNavIdx, setHoverNavIdx] = useState<number | null>(null);
 
   const isUnderDefaultPos = position > 135;
   const isUnderHero = position > 1165;
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     setPosition((prev) => {
-      setTimeout(() => {
-        if (latest > prev) {
-          setIsScrollDown(true);
-        } else {
-          setIsScrollDown(false);
-        }
-      }, 150);
+      if (latest > prev) {
+        setIsScrollDown(true);
+      } else {
+        setIsScrollDown(false);
+      }
 
       return latest;
     });
   });
+
+  const MotionLink = motion(NextLink);
 
   return (
     <>
@@ -35,13 +37,13 @@ const Navigation = () => {
       <div className="h-[50px] bg-system-green-1"></div>
       <motion.div
         animate={{
-          y: isScrollDown && isUnderDefaultPos ? -80 : 0,
+          y: isScrollDown && isUnderDefaultPos ? "-100%" : 0,
           transition: {
-            ease: "easeOut",
+            ease: "easeIn",
             duration: 0.2,
+            delay: 0.1,
           },
           background: isUnderHero ? "white" : undefined,
-          backdropFilter: isUnderDefaultPos ? "blur(10px)" : undefined,
         }}
         className="sticky top-0 bg-system-green-1 z-[100]"
       >
@@ -52,11 +54,29 @@ const Navigation = () => {
           </NextLink>
           {/* NAV (use absolute and make it absolutely center, not rely on flex) */}
           <nav className="absolute flex items-center gap-[52px] justify-between -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            {navigationList.map(({ href, title }, idx) => (
-              <NextLink className="" key={`nav-${idx}`} href={href}>
-                {title}
-              </NextLink>
-            ))}
+            {navigationList.map(({ href, title }, idx) => {
+              const isHovered = idx === hoveredNavIdx;
+
+              return (
+                <li
+                  onMouseEnter={() => setHoverNavIdx(idx)}
+                  onMouseLeave={() => setHoverNavIdx(null)}
+                  key={`nav-${idx}`}
+                  className="relative list-none overflow-hidden"
+                >
+                  <MotionLink className="" href={href}>
+                    {title}
+                  </MotionLink>
+                  <motion.div
+                    animate={{
+                      x: isHovered ? "-20%" : "-110%",
+                      transition: { ease: "easeIn", duration: 0.2 },
+                    }}
+                    className="absolute h-[2px] bottom-0 w-full bg-system-green-2 rounded-lg"
+                  ></motion.div>
+                </li>
+              );
+            })}
           </nav>
           {/* CART AND LOGIN */}
           <div className="flex items-center gap-[57px]">
